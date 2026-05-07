ad687586b06cc19776b508c0a50a09629fceb02b
jasminlei <jasminleinonen@hotmail.com>
2026-05-07 09:39:48 +0300

    exercises 11-12: edit author

--- library-frontend/src/components/Authors.jsx
+++ library-frontend/src/components/Authors.jsx
@@ -1,5 +1,7 @@
 import { gql } from '@apollo/client'
-import { useQuery } from '@apollo/client/react'
+import { useMutation, useQuery } from '@apollo/client/react'
+
+import { useState } from 'react'
 
 const ALL_AUTHORS = gql`
   query {
@@ -12,10 +14,31 @@ const ALL_AUTHORS = gql`
   }
 `
 
+const EDIT_AUTHOR = gql`
+  mutation EditAuthor($name: String!, $setBornTo: Int!) {
+    editAuthor(name: $name, setBornTo: $setBornTo) {
+      name
+      born
+      id
+    }
+  }
+`
+
 const Authors = (props) => {
+  const [name, setName] = useState('')
+  const [born, setBorn] = useState('')
+
   const result = useQuery(ALL_AUTHORS, {
     skip: !props.show,
   })
+
+  const [editAuthor] = useMutation(EDIT_AUTHOR, {
+    refetchQueries: [{ query: ALL_AUTHORS }],
+    awaitRefetchQueries: true,
+  })
+
+  const authors = result.data?.allAuthors ?? []
+  const selectedName = name || ''
 
   if (!props.show) {
     return null
@@ -29,7 +52,22 @@ const Authors = (props) => {
     return <div>error: {result.error.message}</div>
   }
 
-  const authors = result.data?.allAuthors ?? []
+  const submit = async (event) => {
+    event.preventDefault()
+
+    if (!selectedName || born === '') {
+      return
+    }
+
+    await editAuthor({
+      variables: {
+        name: selectedName,
+        setBornTo: Number(born),
+      },
+    })
+
+    setBorn('')
+  }
 
   return (
     <div>
@@ -50,6 +88,36 @@ const Authors = (props) => {
           ))}
         </tbody>
       </table>
+
+      <h3>set birthyear</h3>
+      <form onSubmit={submit}>
+        <div>
+          <label htmlFor='name'>name</label>
+          <select
+            id='name'
+            name='name'
+            value={selectedName}
+            onChange={({ target }) => setName(target.value)}
+          >
+            {authors.map((a) => (
+              <option key={a.name} value={a.name}>
+                {a.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <label htmlFor='born'>born</label>
+          <input
+            id='born'
+            name='born'
+            type='number'
+            value={born}
+            onChange={({ target }) => setBorn(target.value)}
+          />
+        </div>
+        <button type='submit'>update author</button>
+      </form>
     </div>
   )
 }
